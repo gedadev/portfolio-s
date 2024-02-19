153f92ed6bb25489109882f2a016429912593405
Gedaias Jair <gedaias@geda.dev>
2024-02-18 21:16:49 -0600

    Create the footer and the styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>Designed and built by Gedaias Jair</p>
+      <p>
+        Icons by{" "}
+        <a href="https://www.flaticon.com/" target="_blank" rel="noreferrer">
+          Flaticon
+        </a>{" "}
+        and{" "}
+        <a href="https://mui.com/" target="_blank" rel="noreferrer">
+          Material UI Icons
+        </a>
+      </p>
+    </footer>
+  );
+}

fbb6ccd4c2dd581555806c3f2e05f8b9fd9be10d
Gedaias <gedaias@geda.dev>
2025-01-21 14:24:49 -0600

    Refactor project details for E-commerce and Lol Champions Mastery applications

--- src/utils/projectsDetails.js
+++ src/utils/projectsDetails.js
@@ -3,89 +3,67 @@ import { v4 as uuidv4 } from "uuid";
 const projectsDetails = [
   {
     id: uuidv4(),
+    title: "E-commerce Store",
+    technologies: "react, javascript, html, css",
+    description:
+      "An e-commerce application offers utilities like a shopping cart, a product catalog and a login section. With the possibility to add more useful features to enhance the shopping experience.",
+    repo: "https://github.com/gedadev/ecommerce",
+    url: "https://ecommerce-96b.pages.dev",
+    preview: [
+      {
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/ecommerce/1.png",
+        alt: "Home page view",
+      },
+      {
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/ecommerce/2.png",
+        alt: "Products page view",
+      },
+      {
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/ecommerce/3.png",
+        alt: "Product filtering",
+      },
+      {
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/ecommerce/4.png",
+        alt: "Product details page",
+      },
+      {
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/ecommerce/5.png",
+        alt: "Cart page view",
+      },
+    ],
+  },
+  {
+    id: uuidv4(),
     title: "Lol Champions Mastery",
     technologies: "react, javascript, html, tailwindcss",
     description:
-      "This application shows all the champions of a summoner providing the game name and the tag line, showing information like the champion level, the champion points and other information provided by the official Riot Games API.",
+      "This application shows the mastery level of a player's champions, displaying information such as the champion level, the champion mastery points and other data provided by the public Riot Games API.",
     repo: "https://github.com/gedadev/lol-mastery",
     url: "https://lol-mastery.pages.dev/",
     preview: [
       {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/lol-champs-app/screen1.png",
-        alt: "initial form",
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/lol-mastery/1.png",
+        alt: "Home view with search bar and top players",
       },
       {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/lol-champs-app/screen2.png",
-        alt: "response view",
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/lol-mastery/2.png",
+        alt: "Player champions view",
       },
       {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/lol-champs-app/screen3.png",
-        alt: "scrolled menu",
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/lol-mastery/3.png",
+        alt: "Champion stats toggled",
+      },
+      {
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/lol-mastery/4.png",
+        alt: "Champion page details",
+      },
+      {
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/lol-mastery/5.png",
+        alt: "Champion page end",
       },
     ],
   },
-  {
-    id: uuidv4(),
-    title: "Online Store",
-    technologies: "react, javascript, html, css",
-    description:
-      'An online store created with the "fake Store API". I used tools like context API to keep track of the shopping cart in the components that require the interaction with the cart. I build this app with React in the frontend, fetching the data with axios.',
-    repo: "https://github.com/gedadev/shopping-cart",
-    url: "https://shopping-cart-9kh.pages.dev",
-    preview: [
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/cart/screen1.png",
-        alt: "home section preview",
-      },
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/cart/screen2.png",
-        alt: "home section bottom preview",
-      },
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/cart/screen3.png",
-        alt: "products section preview",
-      },
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/cart/screen4.png",
-        alt: "cart preview",
-      },
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/cart/screen5.png",
-        alt: "contact section preview",
-      },
-    ],
-  },
-  {
-    id: uuidv4(),
-    title: "Expenses Manager",
-    technologies: "react, javascript, html, css, mongoDB, express",
-    description:
-      "A react app that tracks the daily expenses and allow the user to classify the expenses by categories, a pay method and if is a recurrent expense. I build this app with React in the frontend, Express in the backend, and mongoDB to store the data.",
-    repo: "https://github.com/gedadev/finance-manager",
-    url: "https://finance-manager.pages.dev/",
-    preview: [
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/expenses/screen1.png",
-        alt: "expenses table preview",
-      },
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/expenses/screen2.png",
-        alt: "add expense modal preview",
-      },
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/expenses/screen3.png",
-        alt: "filters preview",
-      },
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/expenses/screen4.png",
-        alt: "update expense modal preview",
-      },
-      {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/expenses/screen5.png",
-        alt: "manage options preview",
-      },
-    ],
-  },
+
   {
     id: uuidv4(),
     title: "Weather Report App",
@@ -96,15 +74,15 @@ const projectsDetails = [
     url: "https://weather-app-er8.pages.dev/",
     preview: [
       {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/weather/screen1.png",
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/weather/1.png",
         alt: "forecast preview",
       },
       {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/weather/screen2.png",
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/weather/2.png",
         alt: "end slider preview",
       },
       {
-        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/weather/screen3.png",
+        url: "https://pub-1d0f11ab543f45ecb624359bc1a436a7.r2.dev/portfolio%20assets/weather/3.png",
         alt: "search city preview",
       },
     ],
